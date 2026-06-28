29ed8787a82635db6f9ed90b16b446bdf94d0f5c
自律 <110465696+JohnSnow14284@users.noreply.github.com>
2026-06-28 08:19:01 +0800

    Upload NeoForge Voxy port

---
+++ src/main/java/me/cortex/voxy/client/mixin/ClientVoxyMixinPlugin.java
@@ -0,0 +1,54 @@
+package me.cortex.voxy.client.mixin;
+
+import net.neoforged.fml.loading.LoadingModList;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class ClientVoxyMixinPlugin implements IMixinConfigPlugin {
+    private static boolean valkyrienSkiesInstalled;
+    private static boolean nvidiumInstalled;
+    private static boolean connectorInstalled = false;
+
+    private static boolean isLoadedEarly(String modId) {
+        var list = LoadingModList.get();
+        return list != null && list.getModFileById(modId) != null;
+    }
+
+    @Override
+    public void onLoad(String mixinPackage) {
+        valkyrienSkiesInstalled = isLoadedEarly("valkyrienskies");
+        nvidiumInstalled = isLoadedEarly("nvidium");
+        connectorInstalled = isLoadedEarly("connector");
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) { return true; }
+
+    @Override public List<String> getMixins() {
+        List<String> mixins = new ArrayList<>();
+        if (valkyrienSkiesInstalled && !nvidiumInstalled) {
+            mixins.add("sodium.MixinSodiumWorldRendererVS");
+        } else {
+            mixins.add("sodium.MixinDefaultChunkRenderer");
+        }
+
+        return mixins;
+    }
+
+    @Override
+    public String getRefMapperConfig() { return null; }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {}
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {}
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {}
+}
---
+++ src/main/java/me/cortex/voxy/client/mixin/fakesight/MixinIntegratedServer.java
@@ -0,0 +1,26 @@
+package me.cortex.voxy.client.mixin.fakesight;
+
+import me.cortex.voxy.client.config.VoxyConfig;
+import net.minecraft.client.server.IntegratedServer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Mixin(IntegratedServer.class)
+public abstract class MixinIntegratedServer {
+    @ModifyArg(
+            method = "tickServer",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Ljava/lang/Math;max(II)I",
+                    ordinal = 0
+            ),
+            index = 1
+    )
+    private int voxy$modifyIntegratedServerRenderDistance(int originalDistance) {
+        if (VoxyConfig.CONFIG.enableExtendedRequestDistance && VoxyConfig.CONFIG.isRenderingEnabled()) {
+            return VoxyConfig.CONFIG.requestDistance;
+        }
+        return originalDistance;
+    }
+}
